89ae87d83207fcc35385ef463bcc2844f8f349c3
kevinedlyn <c14180208@john.petra.ac.id>
2020-04-29 08:26:03 +0700

    Versi2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,131 +13,169 @@ export class AppComponent  {
   hasil = 0;
   operator = "";
   num1 = false;
+  opeFound = false;
+  num2 = false;
   hasilDone = false;
+  operasi="";
 
-  1()
+  get1()
   {
     if (this.num1 == false)
     {
       this.number1 = 1;
+      this.operasi = this.operasi + this.number1;
       this.num1 = true;
     }
     else
     {
       this.number2 = 1;
+      this.operasi = this.operasi + this.number2;
+      this.num2 = true;
     }
   }
-  2()
+  get2()
   {
     if (this.num1 == false)
     {
       this.number1 = 2;
+      this.operasi = this.operasi + this.number1;
       this.num1 = true;
     }
     else
     {
       this.number2 = 2;
+      this.operasi = this.operasi + this.number2;
+      this.num2 = true;
     }
   }
-  3()
+  get3()
   {
     if (this.num1 == false)
     {
       this.number1 = 3;
+      this.operasi = this.operasi + this.number1;
       this.num1 = true;
     }
     else
     {
       this.number2 = 3;
+      this.operasi = this.operasi + this.number2;
+      this.num2 = true;
     }
   }
-  4()
+  get4()
   {
     if (this.num1 == false)
     {
-      this.number1 = 1;
+      this.number1 = 4;
+      this.operasi = this.operasi + this.number1;
       this.num1 = true;
     }
     else
     {
-      this.number2 = 1;
+      this.number2 = 4;
+      this.operasi = this.operasi + this.number2;
+      this.num2 = true;
     }
   }
-  5()
+  get5()
   {
     if (this.num1 == false)
     {
       this.number1 = 5;
+      this.operasi = this.operasi + this.number1;
       this.num1 = true;
     }
     else
     {
       this.number2 = 5;
+      this.operasi = this.operasi + this.number2;
+      this.num2 = true;
     }
   }
-  6()
+  get6()
   {
     if (this.num1 == false)
     {
       this.number1 = 6;
+      this.operasi = this.operasi + this.number1;
       this.num1 = true;
     }
     else
     {
       this.number2 = 6;
+      this.operasi = this.operasi + this.number2;
+      this.num2 = true;
     }
   }
-  7()
+  get7()
   {
     if (this.num1 == false)
     {
       this.number1 = 7;
+      this.operasi = this.operasi + this.number1;
       this.num1 = true;
     }
     else
     {
       this.number2 = 7;
+      this.operasi = this.operasi + this.number2;
+      this.num2 = true;
     }
   }
-  8()
+  get8()
   {
     if (this.num1 == false)
     {
       this.number1 = 8;
+      this.operasi = this.operasi + this.number1;
       this.num1 = true;
     }
     else
     {
       this.number2 = 8;
+      this.operasi = this.operasi + this.number2;
+      this.num2 = true;
     }
   }
-  9()
+  get9()
   {
     if (this.num1 == false)
     {
       this.number1 = 9;
+      this.operasi = this.operasi + this.number1;
       this.num1 = true;
     }
     else
     {
-      this.number2 = 9;
+      this.number2 = 9;this.num2 = true;
+      this.operasi = this.operasi + this.number2;
+
     }
   }
   plus()
   {
     this.operator = "+";
+    this.operasi = this.operasi + this.operator;
+    this.opeFound = true;
   }
    div()
   {
     this.operator = "/";
+        this.operasi = this.operasi + this.operator;
+    this.opeFound = true;
   }
    mul()
   {
     this.operator = "*";
+        this.operasi = this.operasi + this.operator;
+    this.opeFound = true;
   }
    minus()
   {
     this.operator = "-";
+        this.operasi = this.operasi + this.operator;
+    this.opeFound = true;
   }
   hitung()
   {
@@ -159,5 +197,10 @@ export class AppComponent  {
     }
     this.hasilDone = true;
   }
-
+  reset()
+  {
+    this.hasilDone = false;
+    this.num1 = false;  
+    this.operasi="";
+  }
 }
